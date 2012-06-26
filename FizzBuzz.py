64b07cf930dc83cfbd14137b441570cbd93a74cf
ashishdeshpande <ashishdeshpande89@gmail.com>
2012-06-26 15:56:00 +0530

    Update master

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,11 +1,15 @@
 """
 Q1. Why is the report method untestable ? [2 pts]
 
-
+The report method is using the open command which is an external collaborator, the external collaborator should
+be passed as an argument to the report method
 
 
 Q2. How will you change the api of the report method to make it more testable ? [2 pts]
 
+The defintion of report will accept three arguments report_file(self, numbers,fileWrapper)
+the calls to open , write and close will be substituted by the fileWrapper.
+This wrapper can be mocked and code logic can be tested accordingly.
 
 
 """
